c467bfd12859b6506f75f0a9f97b8acb755eddf2
Andriel Nuernberg <andrielfn@gmail.com>
2018-10-03 18:59:22 -0300

    Format code using the `mix format`

--- mix.exs
+++ mix.exs
@@ -6,8 +6,8 @@ defmodule OpenGraph.Mixfile do
       app: :open_graph,
       version: "0.0.3",
       elixir: "~> 1.3",
-      build_embedded: Mix.env == :prod,
-      start_permanent: Mix.env == :prod,
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: description(),
       package: package()
@@ -33,7 +33,8 @@ defmodule OpenGraph.Mixfile do
   defp deps do
     [
       {:httpoison, "~> 1.0"},
-      {:ex_doc, "~> 0.11", only: :dev}
+      {:ex_doc, "~> 0.11", only: :dev},
+      {:credo, "~> 0.10.0", only: [:dev, :test], runtime: false}
     ]
   end
 
@@ -50,5 +51,5 @@ defmodule OpenGraph.Mixfile do
       links: %{"GitHub" => "https://github.com/andrielfn/open_graph"},
       files: ~w(lib mix.exs README.md LICENSE)
     ]
-    end
+  end
 end
